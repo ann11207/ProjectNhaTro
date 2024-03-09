364123bdc741fdf234eb9020892343cc1ba06242
hello0933 <minhab8232@gmail.com>
2024-03-09 13:16:26 +0700

    2 show password in login

--- app/src/main/java/com/example/projectnhatro/fragments/AccountFragment.java
+++ app/src/main/java/com/example/projectnhatro/fragments/AccountFragment.java
@@ -4,20 +4,50 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.projectnhatro.R;
 
 
 public class AccountFragment extends Fragment {
 
+    CheckBox checkBox_ShowPass;
+    EditText editText_Pass;
+
+    TextView textView_Register;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        // reference CheckBox and EditText
+        checkBox_ShowPass = view.findViewById(R.id.checkbox_ShowPassWord);
+        editText_Pass = view.findViewById(R.id.EditText_PassWord);
+
+
+        //checkbox show password
+        checkBox_ShowPass.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    editText_Pass.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
+                else {
+                    editText_Pass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
+        return view;
+
+
     }
 }
